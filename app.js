71c629a25d18d97a7335d443656b6b46c264f169
Raphael <raphaelbravo190813@gmail.com>
2018-09-28 10:41:35 -0500

    listo

--- app.js
+++ app.js
@@ -3,11 +3,25 @@ function Aplicar () {
 	var elemento2 = document.getElementById("result")
 
 	if (elemento.value != "") {
+        fetch('https://better-ride-api.herokuapp.com/api/organizations', {
+      method: 'post',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({name: elemento.value})
+    
+    }).then(function(response) {
+      return response.json();
+    }).then(function(data) {
         elemento2.className="col-xl-9 mx-auto"
         elemento2.style.backgroundColor = "rgba(0,0,0,0.5)"
         elemento2.style.paddingTop = "50px"
     elemento2.innerHTML =`
-    <h2 class="mb-5">TOKEN: `+generate_token(20)+`</h2>`}
+    <h2 class="mb-5">TOKEN: `+data.id+`</h2>`
+    });
+        
+        }
 }
 function generate_token(length){
     //edit the token allowed characters
