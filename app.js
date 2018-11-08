eb69e6e9b8cbae76909ede8cdcaf63ec2e78cc9a
raphaelbravo190813 <42117861+raphaelbravo190813@users.noreply.github.com>
2018-11-08 14:06:58 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,33 +1,24 @@
 function Aplicar () {
 	var elemento = document.getElementById("nombre")
-	var elemento2 = document.getElementById("result")
-
-	if (elemento.value != "") {
-		var tokengen= generate_token(12)
+	var elemento2 = document.getElementById("email")
+    var elemento3 = document.getElementById("result")
+	if (elemento.value != "" && elemento2.value != "") {
+		//var tokengen= generate_token(12)
         $.ajax({
-            url: "https://vast-meadow-24979.herokuapp.com/organizations",
+            url: "http://srv-desa.eastus2.cloudapp.azure.com/appbetterride/api/v1/organizations",
             type: "POST",
+           
             data: {
                 name: elemento.value,
-                token: tokengen
+                email: elemento2.value
             }
         }).done(function(data){
             data = JSON.parse(data);
             console.log(data);
         })
-        elemento2.className="col-xl-9 mx-auto"
-        elemento2.style.backgroundColor = "rgba(0,0,0,0.5)"
-        elemento2.style.paddingTop = "50px"
-        elemento2.innerHTML =` <h2 class="mb-5">TOKEN: `+tokengen+`</h2>`
+        elemento3.className="col-xl-9 mx-auto"
+        elemento3.style.backgroundColor = "rgba(0,0,0,0.5)"
+        elemento3.style.paddingTop = "50px"
+        elemento3.innerHTML =` <h2 class="mb-5">Se envio el token de registro a su correo</h2>`
         }
-}
-function generate_token(length){
-    //edit the token allowed characters
-    var a = "abcdefghijklmnopqrstuvwxyz1234567890".split("");
-    var b = [];  
-    for (var i=0; i<length; i++) {
-        var j = (Math.random() * (a.length-1)).toFixed(0);
-        b[i] = a[j];
-    }
-    return b.join("");
 }
